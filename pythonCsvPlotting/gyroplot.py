02f0ee8895fcd81950ead33705fdb7d257069a0f
Vincent Bare'encastle <53657534+TheFabulousDeath@users.noreply.github.com>
2024-05-14 20:13:21 +0200

    Update gyroplot.py

--- pythonCsvPlotting/gyroplot.py
+++ pythonCsvPlotting/gyroplot.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import plotly.express as px
 
-#Make sure the csv has column names on the top
-df = pd.read_csv('J:/task1Log_112108_02_05_2024.csv')
-df_melted = df.melt(id_vars='Time', value_vars=['x', 'y', 'z'], var_name='Variable', value_name='Value')
+df = pd.read_csv('J:/task1Log_195605_14_05_2024.csv')
+df_melted = df.melt(id_vars='Time', value_vars=['xAcc', 'yAcc', 'zAcc', 'zGyro'], var_name='Variable', value_name='Value')
 fig = px.line(df_melted, x='Time', y='Value', color='Variable', title='Values over Time')
+
 fig.show()
